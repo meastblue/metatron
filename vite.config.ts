a6627a188e852064dcadb66a9f755bb303d78060
MeastBlue <amalou.massinissa@gmail.com>
2023-02-11 02:42:54 +0100

    start confp

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import preprocess from "svelte-preprocess";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  build: {
+    rollupOptions: {
+      input: "",
+    }
+  },
+  plugins: [svelte({
+    preprocess: preprocess(),
+    compilerOptions: {
+      customElement: true
+    }
+  })],
+});
